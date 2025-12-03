60dc762d12bd5c94091ffee2d06be751f3f721e3
Lilac-Wendy <wendygaken@gmail.com>
2025-12-03 16:57:34 -0300

    CLEAN UP

--- Characters/character_state.gd
+++
@@ -1,8 +0,0 @@
-class_name CharacterState
-extends LimboState
-
-@export var animation_name : StringName
-@export var animation_player: AnimationPlayer
-func _enter() -> void:
-	agent.sprite.play(animation_name)
-	
